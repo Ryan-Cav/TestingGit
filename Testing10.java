c692e557f7bb00c217150b011cdc207b4d2da9dc
Ryan Cavanaugh <ryancav18@gmail.com>
2018-09-21 11:24:16 -0400

    First commit

---
+++ Testing10.java
@@ -0,0 +1,9 @@
+public class Testing10 {
+   public static void main(String[] args){
+    
+      String a = "This is my first git project.";
+      System.out.println(a);
+         
+      
+   }
+}
